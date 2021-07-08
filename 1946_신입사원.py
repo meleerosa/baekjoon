64fe48976ffa882a84024d83e532c44fc69a5b1f
Ji Myeong Park <86149793+meleerosa@users.noreply.github.com>
2021-07-08 17:35:03 +0900

    시간초과 줄이기 위해서 여러 입력 값을 받는 line은 sys.stdin.readline().split()을 써야한다는 사실을 알았다.

--- 1946_신입사원.py
+++ 1946_신입사원.py
@@ -1,29 +1,20 @@
 # 어떤 지원자의 두 성적이 다른 어떤 지원자보다 낮으면 선발되지 않는다.
+# 시간 초과가 나와서 다른 방법으로 생각해보기로 했다. - 차원 수 낮추기, sys.stdin.readline()쓰기
+import sys
 T = int(input())  # test case 의 개수
-grade1 = []  # 서류심사
-grade2 = []  # 면접심사
-
+grade = []
 for i in range(T):
-    grade1.append([])  # 서류심사 케이스 석차 리스트
-    grade2.append([])  # 면접심사 케이스 석차 리스트
+    grade.append([])  # 서류심사 케이스 석차 리스트
     N = int(input())  # 지원자의 숫자
     for j in range(N):  # 지원자의 석차1, 석차2
-        a, b = list(map(int, input().split()))
-        grade1[i].append(a)
-        grade2[i].append(b)
-
-# 각 심사의 가장 높은 석차를 받은 지원자의 다른 심사에서 받은 석차보다
-# 낮은 석차를 기록한 지원자는 무조건 떨어진다.
-# 반례는 겹치는 지원자
-for i in range(T):
-    count = set()
-    index_1 = grade1[i].index(1)
-    index_2 = grade2[i].index(1)
-    for j in range(len(grade2[i])):
-        if grade2[i][index_1] < grade2[i][j]:
-            count.add(j)
-    for j in range(len(grade1[i])):
-        if grade1[i][index_2] < grade1[i][j]:
-            count.add(j)
-    print(len(grade1[i]) - len(count))
-
+        a, b = list(map(int, sys.stdin.readline().split()))
+        grade[i].append([a,b])
+for i in grade:
+    count = 1  # 1등은 미리 포함
+    i.sort(key=lambda x: x[0])  # i가 none type이 되어서 새로 반복문 만듦
+    max = i[0][1]  # 합격할 수 있는 최소 석차
+    for j in i:
+        if max > j[1]:
+            count += 1
+            max = j[1]  # 합격할 수 있는 최소 석차 갱신
+    print(count)
