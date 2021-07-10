4ddef938fe92c7460cb3f87726ceb0d65e69c2e5
Ji Myeong Park <86149793+meleerosa@users.noreply.github.com>
2021-07-10 21:47:29 +0900

    우선순위 큐를 힙으로 구현하는 방식은 처음 알았다. heapq에 대한 메소드를 숙지하자.

--- 1715_카드정렬.py
+++ 1715_카드정렬.py
@@ -1,19 +1,20 @@
 # 반례를 생각하지 못했다 - 합친 카드 팩과 다른 것을 비교할때 다른 두개가 더 작을경우
 # 그 두개 먼저 비교해야하는데 그 경우를 생각하지 못했다.
-
+# 시간복잡도가 너무 크게나와 우선순위 큐를 힙으로 구현하여 사용하기로 했다.
 import sys
+import heapq
 N = int(input())
 data = []
 for i in range(N):
-    data.append(int(sys.stdin.readline()))
-data.sort()
+    heapq.heappush(data, int(sys.stdin.readline()))
+
 result = 0
-for i in range(N):
-    if len(data) >= 2:
-        data.sort(reverse=True)  # 시간복잡도, pop쓰기위해 거꾸로 정렬
-        a = data.pop()
-        b = data.pop()
-        result += a+b
-        data.append(a+b)
-    else: pass
-print(result)
+if len(data) == 1:  # data 가 1개일 경우 비교할 필요 없음
+    print(0)
+else:
+    while len(data) > 1:
+        temp_1 = heapq.heappop(data)
+        temp_2 = heapq.heappop(data)
+        result += temp_1+temp_2
+        heapq.heappush(data, temp_1+temp_2)
+    print(result)
