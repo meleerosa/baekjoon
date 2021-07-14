edf3f092b9a09f50bd5c0e01e7b587e65be2f3ba
Ji Myeong Park <86149793+meleerosa@users.noreply.github.com>
2021-07-15 05:12:18 +0900

    맨 왼쪽 위부터 검사하는 아이디어, numpy 를 빼고 다시 만들어보았다. 2x2 행렬에서는 작동이 되지 않는다.

--- 1080_행렬.py
+++ 1080_행렬.py
@@ -1,5 +1,4 @@
 import sys
-import numpy as np
 # n행 m열의 행렬을 저장하기
 N, M = map(int, sys.stdin.readline().split())
 A = []
@@ -10,24 +9,25 @@ for i in range(N):
 for i in range(N):
     b = sys.stdin.readline().rstrip()
     B.append(list(b))
-A = np.array(A).reshape(N, M)
-B = np.array(B).reshape(N, M)
 
 # x행,y열의 원소부터 3x3 부분 행렬의 원소를 변환하기
 def convert(x, y):
-    for i in range(3):
-        for j in range(3):
-            if A[i+x][j+y] == '1':
-                A[i+x][j+y] = '0'
+    for i in range(x,x+3):
+        for j in range(y,y+3):
+            if A[i][j] == '1':
+                A[i][j] = '0'
             else:
-                A[i+x][j+y] = '1'
+                A[i][j] = '1'
 
 count = 0
 
-while not np.array_equal(A, B):
-    for i in range(N-2):
-        for j in range(M-2):
-            if A[i][j] != B[i][j]:
-                convert(i,j)
-                count += 1
-print(count)
+for i in range(N-2):
+    for j in range(M-2):
+        if A[i][j] != B[i][j]:
+            convert(i,j)
+            count += 1
+            print(A)
+if A == B:
+    print(count)
+else:
+    print(-1)
