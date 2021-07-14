c7de78353be2024c9c1b90d76e9c06578046840f
Ji Myeong Park <86149793+meleerosa@users.noreply.github.com>
2021-07-15 04:52:19 +0900

    맨 왼쪽 위부터 검사하는 아이디어, numpy 를 사용했다. 하지만 백준 사이트에서는 numpy 를 인식하지 못하는 문제가 발생했다.

--- 1080_행렬.py
+++ 1080_행렬.py
@@ -1,29 +1,33 @@
+import sys
+import numpy as np
+# n행 m열의 행렬을 저장하기
+N, M = map(int, sys.stdin.readline().split())
+A = []
+B = []
+for i in range(N):
+    a = sys.stdin.readline().rstrip()
+    A.append(list(a))
+for i in range(N):
+    b = sys.stdin.readline().rstrip()
+    B.append(list(b))
+A = np.array(A).reshape(N, M)
+B = np.array(B).reshape(N, M)
+
+# x행,y열의 원소부터 3x3 부분 행렬의 원소를 변환하기
+def convert(x, y):
+    for i in range(3):
+        for j in range(3):
+            if A[i+x][j+y] == '1':
+                A[i+x][j+y] = '0'
+            else:
+                A[i+x][j+y] = '1'
+
 count = 0
-N,M = map(int,input().split())
 
-change_maps = [list(map(int,input())) for _ in range(N)]
-result_maps = [list(map(int,input())) for _ in range(N)]
-
-# 3X3 행렬을 변환 0->1, 1->0 으로
-def convert3x3(x,y,arr):
-    for i in range(x,x+3):
-        for j in range(y,y+3):
-            change_maps[i][j] = 1 - change_maps[i][j]
-
-for i in range(0,N-2):
-    for j in range(0,M-2):
-        if change_maps[i][j] != result_maps[i][j]: # 배열의 값이 같지 않다
-            convert3x3(i,j,change_maps)
-            count += 1
-
-# 전체 배열 검사
-nonResult = False
-for i in range(0,N):
-    for j in range(0,M):
-        if change_maps[i][j] != result_maps[i][j]:
-            nonReulst = True
-
-if nonResult:
-    print(-1)
-else:
-    print(count)
+while not np.array_equal(A, B):
+    for i in range(N-2):
+        for j in range(M-2):
+            if A[i][j] != B[i][j]:
+                convert(i,j)
+                count += 1
+print(count)
