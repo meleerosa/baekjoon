38f836cd3d07a0d66138ae8edf51e7f00e6b4f83
Ji Myeong Park <86149793+meleerosa@users.noreply.github.com>
2021-07-16 01:06:02 +0900

    이동횟수와 이동 거리가 동시에 증가하는 규칙성 찾음 이동거리를 k-1, k, k+1중에서만 선택할 수 있다는 것을 간과했다.

--- 1011_Fly me to the Alpha Centauri.py
+++ 1011_Fly me to the Alpha Centauri.py
@@ -2,26 +2,18 @@ import sys
 # 테스트 케이스 입력
 T = int(input())
 # 각 테스트 케이스의 현재 위치와 목표 위치 저장
-L = [None]*T
-for i in range(T):
+distance = []
+for _ in range(T):
     x, y = map(int, sys.stdin.readline().split())
-    L[i] = [x,y]
+    distance.append(y-x)  # 각 케이스에 이동해야하는 거리를 저장
 
 for i in range(T):
     count = 0
-    # 작동 시 이동 거리
-    k = 1
-    while True:  # (현재 위치)와 (목표 위치-1) 이 같아지면
-        L[i][0] += k    # count 에 1만 더하고 break
+    k = 1  # 처음 이동 거리
+    total = 0  # 총 이동 거리
+    while total < distance[i]:
         count += 1
-        k += 1
-        if L[i][0] >= L[i][1]:  # 넘어서면
-            count += 1
-            print(count)
-            break
-        elif L[i][0] == L[i][1]-1: # 딱 되면
-            count += 1
-            print(count)
-            break
-
-
+        total += k
+        if count % 2 == 0:
+            k += 1
+    print(count)
